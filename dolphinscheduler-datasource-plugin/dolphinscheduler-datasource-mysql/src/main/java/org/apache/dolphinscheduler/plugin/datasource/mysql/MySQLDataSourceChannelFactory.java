67fb8252f05d9d80e104a853e906740a16e5f4df
mask <Narcasserun@hotmail.com>
2021-12-23 13:35:32 +0800

    add hive mysql provider

--- dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-mysql/src/main/java/org/apache/dolphinscheduler/plugin/datasource/mysql/MySQLDataSourceChannelFactory.java
+++ dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-mysql/src/main/java/org/apache/dolphinscheduler/plugin/datasource/mysql/MySQLDataSourceChannelFactory.java
@@ -20,7 +20,9 @@ package org.apache.dolphinscheduler.plugin.datasource.mysql;
 import org.apache.dolphinscheduler.spi.datasource.DataSourceChannel;
 import org.apache.dolphinscheduler.spi.datasource.DataSourceChannelFactory;
 
-//@AutoService(DataSourceChannelFactory.class)
+import com.google.auto.service.AutoService;
+
+@AutoService(DataSourceChannelFactory.class)
 public class MySQLDataSourceChannelFactory implements DataSourceChannelFactory {
     @Override
     public String getName() {
