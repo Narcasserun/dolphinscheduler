baa333bb931edf61b9ff533d5c7e80e81ab9d50c
mask <Narcasserun@hotmail.com>
2021-12-24 20:40:43 +0800

    update hive pom

--- dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-api/src/main/java/org/apache/dolphinscheduler/plugin/datasource/api/utils/ThreadContextClassLoader.java
+++ dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-api/src/main/java/org/apache/dolphinscheduler/plugin/datasource/api/utils/ThreadContextClassLoader.java
@@ -1,13 +1,17 @@
 package org.apache.dolphinscheduler.plugin.datasource.api.utils;
 
-public class ThreadContextClassLoader {
-    private ClassLoader threadContextClassLoader;
+import java.io.Closeable;
+
+public class ThreadContextClassLoader
+        implements Closeable {
+    private final ClassLoader threadContextClassLoader;
 
     public ThreadContextClassLoader(ClassLoader newThreadContextClassLoader) {
         this.threadContextClassLoader = Thread.currentThread().getContextClassLoader();
         Thread.currentThread().setContextClassLoader(newThreadContextClassLoader);
     }
 
+    @Override
     public void close() {
         Thread.currentThread().setContextClassLoader(threadContextClassLoader);
     }
